06b55afc342183ca8239c0bf551ca1c6bdb9f08c
hamuhamu <hrdng4922000@yahoo.co.jp>
2015-12-30 22:09:26 +0900

    var_dumpを埋め込んだ

--- script/check.sh
+++ script/check.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "こんにちわ！！"
+n=$(ag --file-search-regex '.*\.php' var_dump | tee /dev/tty | wc -l)
+[[ $n >= 1 ]] && {
+    echo "var?dumpが検出された" 1>&2
+    exit 1
+}
+
+echo 'テスト実行'
 phpunit --bootstrap bootstrap.php tests/
+echo 'テスト完了'
--- tests/CalcTest.php
+++ tests/CalcTest.php
@@ -7,6 +7,7 @@ class CalcTest extends PHPUnit_Framework_TestCase
     public function 足し算の計算ができること()
     {
         $calc = new Calc();
+        var_dump($calc);
         $this->assertSame(13, $calc->addition(7, 6));
     }
 }
