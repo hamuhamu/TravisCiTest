6fcbe03a0c1e3126495a5b6efda3cdda90ff7c18
hamuhamu <hrdng4922000@yahoo.co.jp>
2016-01-02 02:32:20 +0900

    var_dumpの削除

--- tests/CalcTest.php
+++ tests/CalcTest.php
@@ -7,7 +7,6 @@ class CalcTest extends PHPUnit_Framework_TestCase
     public function 足し算の計算ができること()
     {
         $calc = new Calc();
-        var_dump($calc);
         $this->assertSame(13, $calc->addition(7, 6));
     }
 }
